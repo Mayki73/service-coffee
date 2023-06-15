5bfda1cc44b4632b0e216170d2733b3e53294137
Anton Simanenkau <antoniestories@gmail.com>
2023-06-15 20:49:57 +0300

    changed

--- functions/sendMail.js
+++ functions/sendMail.js
@@ -29,7 +29,7 @@ exports.handler = async (event) => {
       statusCode: 200,
       headers: {
         "Access-Control-Allow-Origin":
-          "https://master--service-coffee.netlify.app",
+          "https://service-coffee.by",
         "Access-Control-Allow-Headers": "Content-Type",
       },
       body: JSON.stringify({ message: "Email sent successfully" }),
